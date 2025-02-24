dd23abecfd0d60fa55e46c74f15aa84da00ed37c
Solene Brunel <brunel.solene.dev@gmail.com>
2025-02-24 20:58:56 +0100

    :wrench: tailwind fully working

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./templates/**/*.{twig,html.twig}", "./assets/js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
