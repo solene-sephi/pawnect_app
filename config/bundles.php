f63e6e892af2ecf7c7ba87fc4c96b1f3fc8beb59
Solene Brunel <brunel.solene.dev@gmail.com>
2025-02-24 18:57:42 +0100

    :heavy_plus_sign: run npm install

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
 ];
