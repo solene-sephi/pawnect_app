b07b364105769d1a4d0f3ca44cbdc8ef0d238bf1
Solene Brunel <brunel.solene.dev@gmail.com>
2025-02-24 09:06:58 +0100

    :heavy_plus_sign: verify email bundle added

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
