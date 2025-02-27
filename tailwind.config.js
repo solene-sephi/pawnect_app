a26b726ba980e1ee6bee39d14bd7c47a94bb3311
Solene Brunel <brunel.solene.dev@gmail.com>
2025-02-27 21:35:53 +0100

    ::heavy_plus_sign: form theme for Flowbite added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
     "./templates/**/*.{twig,html.twig}",
     "./assets/js/**/*.js",
-    "./node_modules/flowbite/**/*.js", // set up the path to the flowbite package
+    "./node_modules/flowbite/**/*.js",
+    "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin"), // add the flowbite plugin
-  ],
+  plugins: [require("flowbite/plugin")],
 };
