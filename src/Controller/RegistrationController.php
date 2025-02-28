06c0a15f577412bf65dac0ae260443a0558ce1cc
Solene Brunel <brunel.solene.dev@gmail.com>
2025-02-28 22:09:27 +0100

    :sparkles: login added

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -77,7 +77,7 @@ class RegistrationController extends AbstractController
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $user);
         } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $exception->getReason());
+            $this->addFlash('error', $exception->getReason());
 
             return $this->redirectToRoute('app_register');
         }
