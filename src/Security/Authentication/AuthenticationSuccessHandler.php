91b85e8064c89c32fe3ec6fc7f6888221dd50007
Solene Brunel <brunel.solene.dev@gmail.com>
2025-03-01 22:56:36 +0100

    :sparkles: redirect after login for user/shelter handled

--- src/Security/Authentication/AuthenticationSuccessHandler.php
+++ src/Security/Authentication/AuthenticationSuccessHandler.php
@@ -2,17 +2,43 @@
 
 namespace App\Security\Authentication;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Router;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 
 class AuthenticationSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token): JsonResponse
+    use TargetPathTrait;
+
+    public function __construct(
+        private Security $security,
+        private Router $router
+    ) {
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): Response
     {
-        var_dump('ok');
-        die;
-        return new JsonResponse(['apiToken' => $userApiToken]);
+        if ($this->security->isGranted('ROLE_SHELTER_EMPLOYEE') || $this->security->isGranted('ROLE_SHELTER_ADMIN')) {
+            return new RedirectResponse($this->router->generate('app_admin_shelter_dashboard'));
+        }
+
+        // Redirect classic users to the previous page
+        if ($this->security->isGranted('ROLE_USER')) {
+            $targetPath = $this->getTargetPath(
+                $request->getSession(),
+                $this->security->getFirewallConfig($request)
+            );
+
+            if ($targetPath) {
+                return new RedirectResponse($targetPath);
+            }
+        }
+
+        return new RedirectResponse($this->router->generate('app_main'));
     }
 }
