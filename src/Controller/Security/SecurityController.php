fea6e5f3151c066c9b81facb82d70b0ee6dece1b
Solene Brunel <brunel.solene.dev@gmail.com>
2025-03-03 09:31:25 +0100

    :truck: reorganization of controllers and templates

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Security;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,7 +18,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('user/security/login.html.twig', [
+        return $this->render('security/login/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
