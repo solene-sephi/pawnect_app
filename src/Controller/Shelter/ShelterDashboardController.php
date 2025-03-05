16659c98a7915f8e488928b71713d04d0efeebbb
Solene Brunel <brunel.solene.dev@gmail.com>
2025-03-05 21:05:18 +0100

    ✨ Improve sidebar menu structure with dynamic routes

---
+++ src/Controller/Shelter/ShelterDashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Shelter;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+
+#[IsGranted('ROLE_SHELTER_EMPLOYEE')]
+final class ShelterDashboardController extends AbstractController
+{
+    #[Route('/shelter/dashboard', name: 'app_shelter_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('shelter/index.html.twig');
+    }
+}
