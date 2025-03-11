6a1c50559e70cceac31bc57e43a97888fb8d3984
Solene Brunel <brunel.solene.dev@gmail.com>
2025-03-11 19:14:44 +0100

    :recycle: convert all shelter partials to Twig components

--- src/Controller/Shelter/ShelterDashboardController.php
+++ src/Controller/Shelter/ShelterDashboardController.php
@@ -2,23 +2,31 @@
 
 namespace App\Controller\Shelter;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\ShelterService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 #[IsGranted('ROLE_SHELTER_EMPLOYEE')]
 final class ShelterDashboardController extends AbstractController
 {
+
+    public function __construct(private ShelterService $shelterService)
+    {
+    }
+
     #[Route('/shelter/dashboard', name: 'app_shelter_dashboard')]
     public function index(): Response
     {
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
-        $shelter = $user->getShelterEmployee()->getShelter()->getName();
-        dump($user);
-        die;
-        return $this->render('shelter/index.html.twig');
+        $shelter = $this->shelterService->getShelterForLoggedUser();
+
+        return $this->render('shelter/index.html.twig', [
+            'user' => $user,
+            'shelter' => $shelter,
+        ]);
     }
 }
