71ca4037eec1c9c2d3c9f0619e3118ca73485f5e
Solene Brunel <brunel.solene.dev@gmail.com>
2025-03-11 18:28:27 +0100

    :heavy_plus_sign: twig component bundle added

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     TalesFromADev\FlowbiteBundle\TalesFromADevFlowbiteBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
