6951b63a1621a8fcc5d7438a8dbba48f6641fe18
Solene Brunel <brunel.solene.dev@gmail.com>
2025-03-19 14:59:49 +0100

    🛠️  add constraints to Blameable trait

--- src/Entity/Trait/BlameableTrait.php
+++ src/Entity/Trait/BlameableTrait.php
@@ -5,6 +5,7 @@ namespace App\Entity\Trait;
 use App\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Warning : add #[HasLifecycleCallbacks] annotation for each entity implementing this trait
@@ -13,10 +14,15 @@ trait BlameableTrait
 {
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
+    #[Assert\NotNull]
+    #[Assert\Valid]
+    #[Assert\Type(User::class)]
     private ?User $created_by = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: true)]
+    #[Assert\Valid]
+    #[Assert\Type(User::class)]
     private ?User $updated_by = null;
 
     public function getCreatedBy(): ?User
