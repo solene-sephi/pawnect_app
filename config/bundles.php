2c91fe2697a5dd7c897b4aa878f856de75b73bed
Solene Brunel <brunel.solene.dev@gmail.com>
2025-04-01 09:43:16 +0200

    ➕ pagerfanta added

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
