390240726fa6af731b5a8a2670e8c0f2e770873f
Solene Brunel <brunel.solene.dev@gmail.com>
2025-04-01 09:25:26 +0200

    🎨 improve enum values display logic

--- src/Entity/Enum/AnimalSexEnum.php
+++ src/Entity/Enum/AnimalSexEnum.php
@@ -7,4 +7,12 @@ enum AnimalSexEnum: string
     case MALE = 'male';
     case FEMALE = 'female';
     case NOT_APPLICABLE = 'not applicable';
+
+    public function getLabel(bool $isFront = false): string
+    {
+        return match ($this) {
+            self::NOT_APPLICABLE => $isFront ? '-' : ucfirst($this->value),
+            default => ucfirst($this->value),
+        };
+    }
 }
